fae2ed3fd410085de3bf0722ea9777528a4cdbae
Maksimna <121045499+Maksimna@users.noreply.github.com>
2022-12-19 18:50:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const student = "Носов Максим Владимирович";
+
+document.getElementById("student").innerHTML = student;
+
+let a;
+let b;
+let c = prompt('Введите счёт первой команды');
+let d = prompt('Введите счёт второй команды');
+
+a = 1;
+b = 2;
+
+
+
+if (a == c, b == d) {
+   alert( 'Счёт был угадан верно!' )
+} else if (a > b, c > d) {
+   alert( 'Счёт не угадали, но исход угадали' )
+} else if ( a < b, c < d) {
+   alert( 'Счёт не угадали, но исход угадали' )
+} else if ( a == b, c == d) {
+   alert( 'Счёт не угадали, но исход угадали' )
+} else {
+   alert( 'Ни счёта, ни исхода не угадали' )
+}
+
